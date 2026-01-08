4820693916fdf922c282a3cb9a763269ee405846
moririnBass <motchin6969@gmail.com>
2026-01-08 13:38:13 +0000

    add bow count component

--- src/components/contents/Controller.jsx
+++ src/components/contents/Controller.jsx
@@ -1,8 +1,11 @@
 import RootSelector from "./RootSelector";
+import BowCountSelector from "./BowCountSelector";
+
 function Controller({ changeRootSound, changeBowCount }) {
   return (
     <>
       <RootSelector changeRootSound={changeRootSound} />
+      <BowCountSelector changeBowCount={changeBowCount} />
     </>
   );
 }
