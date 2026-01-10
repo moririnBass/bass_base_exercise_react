ae78fa66d5b7111b96754d77b8f838c30430b13c
moririnBass <motchin6969@gmail.com>
2026-01-10 00:18:16 +0000

    add chord symbol & mod hide logic

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,17 +1,17 @@
 // サウンドノートの定数
 export const SOUND_NOTES = [
-  { seq: 1, name: "C" },
-  { seq: 2, name: "C#/Db" },
-  { seq: 3, name: "D" },
-  { seq: 4, name: "D#/Eb" },
-  { seq: 5, name: "E" },
-  { seq: 6, name: "F" },
-  { seq: 7, name: "F#/Gb" },
-  { seq: 8, name: "G" },
-  { seq: 9, name: "G#/Ab" },
-  { seq: 10, name: "A" },
-  { seq: 11, name: "A#/Bb" },
-  { seq: 12, name: "B" },
+  { seq: 0, name: "C" },
+  { seq: 1, name: "C#/Db" },
+  { seq: 2, name: "D" },
+  { seq: 3, name: "D#/Eb" },
+  { seq: 4, name: "E" },
+  { seq: 5, name: "F" },
+  { seq: 6, name: "F#/Gb" },
+  { seq: 7, name: "G" },
+  { seq: 8, name: "G#/Ab" },
+  { seq: 9, name: "A" },
+  { seq: 10, name: "A#/Bb" },
+  { seq: 11, name: "B" },
 ];
 
 export const BOWS = [
@@ -24,7 +24,30 @@ export const BOWS = [
 ];
 
 export const CODE_TONES = [
-  { seq: 1, chordName: "Major", interval: "4,7" }, // Major
-  { seq: 2, chordName: "minor", interval: "3,7" }, // Minor
-  // 他のコードトーンを追加可能
+  // 1. 三和音（トライアド）
+  { seq: 1, chordName: "Major:メジャー", interval: [4, 7] },
+  { seq: 2, chordName: "minor:マイナー", interval: [3, 7] },
+  { seq: 3, chordName: "dim:ディミニッシュ", interval: [3, 6] },
+  { seq: 4, chordName: "aug:オーグメント", interval: [4, 8] },
+  { seq: 5, chordName: "sus4:サスペンデッド・フォー", interval: [5, 7] },
+  // 2. 四和音（セブンス・コード）
+  { seq: 6, chordName: "M7:メジャー・セブンス", interval: [4, 7, 11] },
+  { seq: 7, chordName: "m7:マイナー・セブンス", interval: [3, 7, 10] },
+  { seq: 8, chordName: "7:ドミナント・セブンス", interval: [4, 7, 10] },
+  {
+    seq: 9,
+    chordName: "mM7:マイナー・メジャー・セブンス",
+    interval: [3, 7, 11],
+  },
+  {
+    seq: 10,
+    chordName: "m7-5:マイナー・セブンス・フラットファイブ",
+    interval: [3, 6, 10],
+  },
+  { seq: 11, chordName: "dim7:ディミニッシュ・セブンス", interval: [3, 6, 9] },
+  // 3. テンション・その他
+  { seq: 12, chordName: "add9:アドナインス", interval: [2, 4, 7] },
+  { seq: 13, chordName: "6:シックス", interval: [4, 7, 9] },
+  { seq: 14, chordName: "m6:マイナー・シックス", interval: [3, 7, 9] },
+  { seq: 15, chordName: "7sus4:セブン・サスフォー", interval: [5, 7, 10] },
 ];
